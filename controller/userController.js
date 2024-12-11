f91d10465a0deb4c9f2ec1ff84a11227e947c5e2
Victor-MGB <mgbemenaosonduv@gmail.com>
2024-12-11 09:40:21 +0000

    addedd

--- controller/userController.js
+++ controller/userController.js
@@ -97,7 +97,56 @@ const loginUser = async (req, res) => {
   }
 };
 
+const verifyEmail = async (req, res) => {
+  const { token } = req.params;
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.verificationStatus.emailVerified) {
+      return res.status(400).json({ message: "Email already verified" });
+    }
+
+    // Update the user's email verification status
+    user.verificationStatus.emailVerified = true;
+    await user.save();
+
+    res.status(200).json({ message: "Email successfully verified" });
+  } catch (error) {
+    console.error("Verification error:", error);
+    res.status(400).json({ message: "Invalid or expired token" });
+  }
+};
+
+
+// Get User Profile
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password -security.secretAnswer");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      message: "User profile fetched successfully",
+      user,
+    });
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  verifyEmail,
+  getUserProfile
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controller/userController");
+const { registerUser, loginUser, verifyEmail } = require("../controller/userController");
+const { authenticateToken } = require("../middleware/authMiddleware");
+const { getUserProfile } = require("../controller/userController"); // Ensure this path is correct
 
 const router = express.Router();
 
 // Register a new user
 router.post("/register", registerUser);
 router.post("/login", loginUser)
+router.get("/verify-email/:token", verifyEmail);
+router.get("/profile",authenticateToken, getUserProfile);
 
 module.exports = router;
